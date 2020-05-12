95dd9059a9f553b97a55740d545383fa6f95cace
Alejandro Mejia <alejaso15@hotmail.com>
2020-05-12 22:33:24 +0200

    Add project files.

---
+++ KafkaListener/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace KafkaListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<KeyValuePair<string, string>> config = BuildConsumerConfig();
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("logistics.instruction.job-state-change-dev");
+
+                while (true)
+                {
+                    var consumeResult = consumer.Consume(5000);
+
+                    if (consumeResult != null)
+                    {
+                        consumer.Commit(consumeResult);
+                        Console.WriteLine($"message consumed: \n {consumeResult.Message.Value}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No messages. Trying again");
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<KeyValuePair<string, string>> BuildConsumerConfig()
+        {
+            return new ConsumerConfig
+            {
+                BootstrapServers = "test",
+                SaslMechanism = SaslMechanism.Plain,
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslUsername = "test",
+                SaslPassword = "test",
+                GroupId = "ALESCTESTCONSUMER",
+                EnableAutoCommit = false
+            };
+        }
+    }
+}
